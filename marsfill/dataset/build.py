eee26db02b061e4fd10b10800b710ab402f7c686
Bruno R. Holanda <brunorodriguesholanda@gmail.com>
2025-10-30 17:05:50 -0300

    Add new images and papers to documentation; comment out file removal in build process

--- marsfill/dataset/build.py
+++ marsfill/dataset/build.py
@@ -103,7 +103,7 @@ class Build:
             creationOptions=['COMPRESS=LZW', 'BIGTIFF=YES'],
         )
 
-        os.remove(dtm)
+        # os.remove(dtm)
 
     def _normalize_pair(self, ortho_tile_arr, dtm_tile_arr):
         """Normaliza um par de blocos (tiles) já lidos do disco."""
@@ -308,6 +308,6 @@ class Build:
                     shutil.rmtree(tiles_dir)
                 raise e
             finally:
-                if download_sources.exists():
-                    shutil.rmtree(download_sources)
+                # if download_sources.exists():
+                    # shutil.rmtree(download_sources)
                 count += 1
